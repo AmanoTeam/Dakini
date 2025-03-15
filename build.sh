d78388b0f8d800ae35c9e37c7f3a21a6506f5b15
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-03-14 23:26:51 -0300

    Tune GCC configure options

--- build.sh
+++ build.sh
@@ -169,8 +169,8 @@ if ! [ -f "${binutils_tarball}" ]; then
 		patch --directory="${binutils_directory}" --strip='0' --input="${name}"
 	done
 	
-	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/patches/0001-Revert-gold-Use-char16_t-char32_t-instead-of-uint16_.patch"
-	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/patches/0001-Disable-annoying-linker-warnings.patch"
+	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Revert-gold-Use-char16_t-char32_t-instead-of-uint16_.patch"
+	patch --directory="${binutils_directory}" --strip='1' --input="${workdir}/submodules/obggcc/patches/0001-Disable-annoying-linker-warnings.patch"
 fi
 
 if ! [ -f "${gcc_tarball}" ]; then
